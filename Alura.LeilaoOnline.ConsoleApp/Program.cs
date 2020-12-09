19f69521531d4f1291e62807e4c0407c31030dba
Romaquis <romaquis.ribeiro@jnmoura.com.br>
2020-12-09 11:16:53 -0300

    Projeto Inicial. Criado projeto de testes com dois testes

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            //LeilaoComVariosLances();
+            //LeilaoComApenasUmLance();
+            
+        }
+
+        public void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}.");
+            }
+            Console.ForegroundColor = cor;
+        }
+
+    }
+}
