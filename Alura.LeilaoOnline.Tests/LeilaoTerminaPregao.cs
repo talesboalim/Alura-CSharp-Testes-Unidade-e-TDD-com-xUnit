1a0ffd5f36c382cb0eb98aaae1c93b93bdda7fb6
talesboalim <talesboalim@gmail.com>
2020-12-09 19:40:11 -0300

    Incluído novos testes com Fact e Theory passando múltiplos testes

---
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,46 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        
+        [Theory]
+        [InlineData (1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData (1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData (800, new double[] { 800 })]
+
+        public void LeilaoComVariosLances(
+            double valorEsperado,
+            double[] ofertas)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano",leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            leilao.TerminaPregao();
+
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        [Fact]
+        public void LeilaoSemLances()
+        {
+            var leilao = new Leilao("Van Gogh");
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
